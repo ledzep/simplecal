9cd608bdd58b73c521f6ed82c4f22a57361e7dde
Atul <amkcat9@gmail.com>
2013-07-11 15:17:31 +0530

    configuration file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "atul";
+$dbpassword = "mypasswd";
+$dbdatabase = "simplecal";
+
+$config_name = "Calendar";
+
+$config_basedir = "http://127.0.0.1/sites/simplecal/";
+
+?>
